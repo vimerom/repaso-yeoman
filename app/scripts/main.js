d112a1d9d4261d878381986b5527a184e35bf862
victor <vimerom@gmail.com>
2015-04-30 13:11:01 +0200

    ds

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+'use strict'
+
+$("#enviar").click(function() {
+    var $boton = $(this);
+    var promesa = $.ajax({
+        url: 'http://www.media.formandome.es/phonegap/tutorial/futbolistas.php',
+        dataType: 'json'
+    });
+
+    promesa.done(function(futbolistas) {
+        console.log('success');
+        $boton.css('display', 'none');
+        //$.each(array/object, function(index, val) {
+        /* iterate through array or object */
+        //});
+
+
+        var filas;
+        $.each(futbolistas, function(index, futbolista) {
+            var fila;
+            $.each(futbolista, function(index, val) {
+                fila += '<td>' + val + '</td>';
+            });
+            filas += '<tr>' + fila + '</tr>'
+        });
+        $('#listado tbody').append(filas);
+        $("#listado").css('display', 'block');
+    });
+    promesa.fail(function() {
+        console.log('error');
+    });
+
+
+});
